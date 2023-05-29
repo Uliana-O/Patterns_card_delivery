989ce3b85ab88f32a9e1dca37fbc4f210af019eb
Uliana <uosintseva@gmail.com>
2023-05-29 20:40:03 +0300

    add files

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,17 +1,22 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
 
     @BeforeEach
     void setup() {
-        open("http://localhost:9999");
+        open("http://localhost:7777");
     }
 
     @Test
@@ -22,10 +27,48 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+        class DeliveryCardTest {
+
+            @BeforeEach
+            void setup() {
+                open("http://localhost:7777");
+            }
+
+            @Test
+            @DisplayName("Should successful plan and changePlan meeting")
+            void shouldSuccessfulPlanAndChangePlanMeeting() {
+                var validUser = DataGenerator.Registration.generateUser("ru");
+                var daysToAddForFirstMeeting = 4;
+                var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+                var daysToAddForSecondMeeting = 7;
+                var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+                // первичная дата
+
+                $("[data-test-id=city] input").setValue(validUser.getCity());
+                $(".calendar-input input").doubleClick().sendKeys(Keys.BACK_SPACE);
+                $(".calendar-input input").setValue(firstMeetingDate);
+                $("[data-test-id=name] input").setValue(validUser.getName());
+                $("[data-test-id=phone] input").setValue(validUser.getPhone());
+                $("[data-test-id='agreement']").click();
+                $(".button").click();
+                $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(30));
+                $("[data-test-id=success-notification] .notification__content")
+                        .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
+
+                //перепланирование
+
+                $(".calendar-input input").doubleClick().sendKeys(Keys.BACK_SPACE);
+                $(".calendar-input input").setValue(secondMeetingDate);
+                $(".button").click();
+                $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(30));
+                $("[data-test-id= changePlan-notification]")
+                        .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+                $(withText("Перепланировать")).click();
+                $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(30));
+                $("[data-test-id=success-notification] .notification__content")
+                        .shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate));
+            }
+        }
     }
 }
